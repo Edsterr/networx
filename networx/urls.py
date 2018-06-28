aba7be70ba42b36c2046d4f9d89cb94c450bb205
osboxes.org <horia_zegheru@yahoo.com>
2018-06-28 16:10:52 +0300

    added urls

--- networx/urls.py
+++ networx/urls.py
@@ -23,5 +23,8 @@ from django.urls import path
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
     path('index/', views.index, name='index'),
+    path('search/', views.search, name='search'),
+    path('profile/', views.profile, name='profile'),
+    path('register/', views.register, name='register'),
     path('admin/', admin.site.urls),
 ]
